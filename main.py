fcdeb499ab7d8e94e8b8502f6aeda297837ae733
Cameron B <x0p30d3@gmail.com>
2015-05-01 12:14:14 +1000

    modified:   main.py Fixed display issues resuming to terminal on 	exit. Removed some pointless string/list manipulation. Fixed 	player class referring to itself by name.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 # (c) Cameron Bland
 # http://github.com/itsdogtime/-y-bird
@@ -7,6 +7,23 @@ import os
 from random import randint
 from time import sleep
 import curses
+
+class CursesScreen():
+	def __enter__(self):
+		self.stdscr = curses.initscr()
+		curses.cbreak()
+		curses.noecho()
+		self.stdscr.keypad(1)
+		self.stdscr.nodelay(1)
+		return self.stdscr
+
+	def __exit__(self,a,b,c):
+		curses.nocbreak()
+		self.stdscr.keypad(0)
+		stdscr.clear()
+		stdscr.refresh()
+		curses.echo()
+		curses.endwin()
 
 class Player():
 	def __init__(self):
@@ -26,42 +43,39 @@ class Player():
 			if self.vcap > 3:
 				self.vel = 3
 
-		player.y += player.vel
+		self.y += self.vel
 
 	def flap(self):
-		player.vel = -3
+		self.vel = -3
 
 	def gravity(self):
 		self.vel += 1
 
 class Walls():
-	def __init__(self):
+	def __init__(self,res,player_x):
+		self.res = res
 		self.tile = "|"
 		self.space_size = 15
 		self.hole_size = 2
 		self.edge_buffer = 2 + self.hole_size
-		self.walls = []
-		self.generate_walls()
-
-		# Check how far into space_size we should be initially
+		self.generate_walls(player_x)
 		self.step = 0
-		for i in self.walls[::-1]:
-			if i:
-				break
-			self.step += 1
 
 	def __getitem__(self,i):
 		return self.walls[i]
 
-	def generate_walls(self): # returns a list of ints. the list[pos] is the wall itself, the number is the hole
-		self.walls = [randint(2,res["h"]-self.edge_buffer) if x % self.space_size == 0 else 0 for x in xrange(0,res["w"])]
+	def generate_wall(self):
+		return randint(self.edge_buffer,self.res["h"]-self.edge_buffer)
 
-	def increment_walls(self):
+	def generate_walls(self,p_x): # sets self.walls as list of ints. the list[pos] is the wall itself, the number is height of the hole
+		self.walls = [self.generate_wall() if x % self.space_size == 0 and x > p_x + self.space_size else 0 for x in xrange(0,self.res["w"])]
+
+	def update(self):
 		self.step += 1
 		self.walls.pop(0)
 
-		if self.step == self.space_size:
-			self.walls.append(randint(self.edge_buffer,res["h"]-self.edge_buffer))
+		if self.step >= self.space_size:
+			self.walls.append(self.generate_wall())
 			self.step = 0
 		else:
 			self.walls.append(0)
@@ -70,44 +84,36 @@ def get_res():
 	h,w = stdscr.getmaxyx()
 	return {"w" : w, "h": h}
 
-def build_map(res):
-	line = [" " for x in xrange(res["w"])]
-	cmap = [line for x in xrange(res["h"])]
-	return cmap
 
-def print_map(cmap, walls):
-	max_height = len(cmap)
-	max_width = len(cmap[0])
+def draw_map(res, player, walls):
 
 	mapstring = []
 
-	for i_y,y in enumerate(cmap): # iter over y
-		for i_x,x in enumerate(y):# iter over x
+	for y in xrange(res['h']):
+		for x in xrange(res['w']):
 
-			if i_y == max_height - 1 and i_x == max_width - 1: 
+			if y == res['h'] - 1 and x == res['w'] - 1: 
 				break # Do not write to last character in screen or curses will abort (ERR)
-
-			if i_x == 2 and i_y == 12:
+			if x == 2 and y == res['h'] / 2:
 				mapstring.append("Score: %03d" % player.score) # print the score
-			elif i_x > 2 and i_x < 12 and i_y == 12: #don't print 9 chars to account for adding 10 in 1 iter
+			elif x > 2 and x < 12 and y == res['h'] / 2: #don't print 9 chars to account for adding 10 in the score
 				pass
-			elif player.x == i_x and player.y == i_y:
+			elif player.x == x and player.y == y:
 				mapstring.append(player.tile)
-			elif walls[i_x]:
-				if i_y < walls[i_x] - walls.hole_size or i_y > walls[i_x] + walls.hole_size:
+			elif walls[x]:
+				if y < walls[x] - walls.hole_size or y > walls[x] + walls.hole_size:
 					mapstring.append(walls.tile)
 				else:
-					mapstring.append(x)
+					mapstring.append(' ')
 			else:
-				# stdscr.addstr(x)
-				mapstring.append(x)
+				mapstring.append(' ')
 
 	return "".join(mapstring)
 
 
-def collision_detect():
+def collision_detect(player,walls):
 	collision = False
-	if walls[player.x]:
+	if walls[player.x]: # only check on non-zero value (a wall)
 		if player.y < walls[player.x] - walls.hole_size or player.y > walls[player.x] + walls.hole_size:
 			collision = True # if player is not in walls.hole_size
 		else:
@@ -115,31 +121,30 @@ def collision_detect():
 	return collision
 
 
-def main(fps):
-	global player
-	global walls
-	cmap = build_map(res)
+def main_loop(fps):
+	player = Player()
+	res = get_res()
+	walls = Walls(res,player.x)
+
 
 	while True:
 
 		p_input = stdscr.getch()
 
 		if p_input == ord('q') or p_input == ord('Q'):
-			stdscr.clear()
-			stdscr.refresh()
 			exit(0)
 		elif p_input == ord('r') or p_input == ord('R'):
 			player = Player()
-			walls = Walls()
+			walls = Walls(res,player.x)
 		elif p_input == ord(' '):
 			player.move(True)
 		else:
 			player.move(False)
 
 		if not player.dead:
-			walls.increment_walls()
-			player.dead = collision_detect()
-			frame = print_map(cmap, walls)
+			walls.update()
+			player.dead = collision_detect(player,walls)
+			frame = draw_map(res, player, walls)
 		else:
 			frame = ["Game Over. Thanks Obama.\n",
 			"Final score = %03d\n"% player.score,
@@ -155,13 +160,6 @@ def main(fps):
 
 
 if __name__ == "__main__":
-
-	stdscr = curses.initscr()
-	stdscr.keypad(1)
-	stdscr.nodelay(1)
-
-	fps = .1
-	res = get_res()
-	walls = Walls()
-	player = Player()
-	main(fps)
+	with CursesScreen() as stdscr:
+		fps = .1
+		curses.wrapper(main_loop(fps))
